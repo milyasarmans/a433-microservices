6f174da000d97f379de333f9faa356c469815fa0
M Ilyas Arman S <83935008+milyasarmans@users.noreply.github.com>
2023-07-01 20:37:12 +0700

    Create build_push_image_karsajobs.sh

---
+++ karsajobs/build_push_image_karsajobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# membuat Docker image dari Dockerfile yang tadi dibuat
+docker build -t karsajobs:latest .
+
+# melihat daftar image
+docker image ls
+
+# mengubah nama image agar sesuai dengan format Docker Hub
+docker tag karsajobs-ui:latest milyasarmans/karsajobs:latest
+
+# login ke Docker Hub
+docker login
+
+# mengunggah image ke Docker Hub 
+docker push milyasarmans/karsajobs:latest
